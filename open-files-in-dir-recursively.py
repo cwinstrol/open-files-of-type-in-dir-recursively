7a4e55d2fe09e878ba9974a0d8e513811577757c
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2021-12-16 22:10:45 -0500

    Update open-files-in-dir-recursively.py

--- open-files-in-dir-recursively.py
+++ open-files-in-dir-recursively.py
@@ -1,6 +1,6 @@
 import os
 import glob
-dir = '' #edit this string to name the directory to be recursively searched (leaving this string empty may crash your system, depending)
-ext = 'txt' #editable for other file extension types such as pdf, psd, etc.
+dir = '' #directory to be recursively searched
+ext = 'txt' #file extension - pdf, psd, etc.
 for file in glob.iglob(dir + '**/*.' + ext, recursive=True):
   os.startfile(file)
